2904ee01442c0d447b7c09ac84187e9c7e4d8ee0
Gavin <w242235718@outlook.com>
2023-02-20 16:14:07 +0800

    first commit

---
+++ FishOnline/pages/index/index.vue
@@ -0,0 +1,19 @@
+<template>
+	<view class="container">
+		{{hello}}
+	</view>
+</template>
+
+<script setup>
+import { ref } from "vue"
+
+const hello = new ref("Hello Wechat App")
+</script>
+
+<style>
+	.container {
+		padding: 20px;
+		font-size: 14px;
+		line-height: 24px;
+	}
+</style>
